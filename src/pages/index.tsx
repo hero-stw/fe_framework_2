d14e287fbf37ef22be3b2231acf7a277788d1f2d
hero-stw <92532202+hero-stw@users.noreply.github.com>
2022-07-22 14:34:45 +0700

    Develop (#3)
    * Update PJ Base
    
    * Update PJ Base
    
    * Add Project Base
    
    Co-authored-by: Pham Anh Hung <hero.ulis.50@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+
   return (
     <div>
       HomePage
@@ -8,4 +9,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
