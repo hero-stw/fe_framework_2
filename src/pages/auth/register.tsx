78c982ab6cc8e5dd49ddfa82d780703816ea44fc
nguyenvanhieu <hieunvph15760@fpt.edu.vn>
2022-07-23 21:01:06 +0700

    FA-5 update login-register

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,35 +1,108 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useForm, SubmitHandler} from "react-hook-form"
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 type Props = {}
 
+type typeRegister = {
+    name:string,
+    email:string,
+    password:string
+}
+
 const Register = (props: Props) => {
+
+const[meraki,setMeraki] = useState(false);
+
+const router = useRouter()
+
+const {register,handleSubmit,formState:{errors}} = useForm<typeRegister>();
+
+const onSubmit:SubmitHandler<typeRegister> = async (value) =>{
+    console.log(value);
+    if(value){
+        setMeraki(true)
+        setTimeout(()=>{
+            setMeraki(false)
+            router.push('/auth/login');
+        },3000)
+    }
+}
+
   return (
-    <div className='w-full bg-[#1F2937] pt-20' style={{height:'640px'}}>
+    <div className='w-full relative bg-[#1F2937] pt-10' style={{height:'700px'}}>
+        {meraki ? 
+        <div className="flex right-2 absolute w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex items-center justify-center w-12 bg-emerald-500">
+            <svg className="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z"/>
+            </svg>
+        </div>
+        
+        <div className="px-4 py-2 -mx-3">
+            <div className="mx-3">
+                <span className="font-semibold text-emerald-500 dark:text-emerald-400">Success</span>
+                <p className="text-sm text-gray-600 dark:text-gray-200">Your account has been registered, go to the login page in 3 seconds !</p>
+            </div>
+        </div>
+        </div>
+         : ""}
         <div className="flex flex-col max-w-md p-6 py-64 rounded-md sm:p-10 bg-gray-900 text-gray-100 m-auto">
             <div className="mb-8 text-center">
-                <h1 className="my-3 text-4xl font-bold">Sign in</h1>
-                <p className="text-sm text-gray-400">Sign in to access your account</p>
+                <h1 className="my-3 text-4xl font-bold">Sign up</h1>
+                <p className="text-sm text-gray-400">Sign up to access your account</p>
             </div>
-            <form action="" className="space-y-12 ng-untouched ng-pristine ng-valid ">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-12 ng-untouched ng-pristine ng-valid">
                 <div className="space-y-4">
                     <div>
+                        <label htmlFor="username" className="block mb-2 text-sm">Username</label>
+                        <input type="text" id="username" {...register('name', 
+                        {
+                            required:"Name cannot be blank !", 
+                            minLength: {
+                                value:3,
+                                message:"Name should not be less than 3 characters !"
+                            },
+                            maxLength:{
+                                value:30,
+                                message:"Name cannot be more than 30 characters !"
+                            }
+                            })} className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
+                        <span className='text-red-500'>{errors.name?.message}</span>
+                    </div>
+                    <div>
                         <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
-                        <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
+                        <input type="text" id="email" {...register('email',
+                        {
+                            required:"Email cannot be blank !" ,
+                            pattern:{
+                                value:/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                message:"Your email is not in the correct format !"
+                            }
+                        })} className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
+                        <span className='text-red-500'>{errors.email?.message}</span>
                     </div>
                     <div>
                         <div className="flex justify-between mb-2">
                             <label htmlFor="password" className="text-sm">Password</label>
-                            <a rel="noopener noreferrer" href="#" className="text-xs hover:underline text-gray-400">Forgot password?</a>
                         </div>
-                        <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
+                        <input type="password" id="password" {...register('password',{ 
+                            required:"Name cannot be blank !", 
+                            minLength: {
+                                value:3,
+                                message:"Password should not be less than 3 characters !"
+                            },
+                            })} className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
+                        <span className='text-red-500'>{errors.password?.message}</span>
                     </div>
                 </div>
                 <div className="space-y-2">
                     <div>
-                        <button type="button" className="w-full px-8 py-3 font-semibold rounded-md bg-violet-400 text-gray-900">Sign in</button>
+                        <button type="submit" className="w-full px-8 py-3 font-semibold rounded-md bg-violet-400 text-gray-900">Sign up</button>
                     </div>
-                    <p className="px-6 text-sm text-center text-gray-400">Dont have an account yet?
-                        <a rel="noopener noreferrer" href="#" className="hover:underline text-violet-400">Sign up</a>.
+                    <p className="px-6 text-sm text-center text-gray-400">Do you already have an account ?
+                        <span className="hover:underline text-violet-400"> <Link href="/auth/login">Signin</Link></span>.
                     </p>
                 </div>
             </form>
