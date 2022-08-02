adf67cb1e0e43c18956884c3fbfe06362e0f4e9d
vasloc202 <nvloc2510z@gmail.com>
2022-08-02 18:17:12 +0700

    FA-50 icon leaderboard

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import style from "@/components/Leaderboard/Leaderboard.module.css"
 import Image from 'next/image';
-import {useRecords} from "../../hooks/records";
+import { useRecords } from "../../hooks/records";
 import goldmedal from "@/img/gold-medal.png";
 import silvermedal from "@/img/silver-medal.png";
 import bronzemedal from "@/img/bronze-medal.png";
@@ -13,11 +13,11 @@ const LeaderBoard = (props: Props) => {
     const [sidebar, setSidebar] = useState(true);
     const showSidebar = () => setSidebar(!sidebar);
 
-    const[users,setUsers] = useState<any>([]);
+    const [users, setUsers] = useState<any>([]);
 
-    const {data:records} = useRecords();
+    const { data: records } = useRecords();
 
-    const sortUsers = (index) =>{
+    const sortUsers = (index) => {
 
         // if(index == 0) {
         //     return <Image src={goldmedal}></Image>
@@ -37,19 +37,19 @@ const LeaderBoard = (props: Props) => {
             case 2:
                 return <Image src={bronzemedal}></Image>
             default:
-                return <div className='bg-gray-500 rounded-full h-8 w-8 flex justify-center items-center ml-2 text-white'>{++index}</div> ;
+                return <div className='bg-gray-500 rounded-full h-8 w-8 flex justify-center items-center ml-2 text-white'>{++index}</div>;
         }
     }
 
-    useEffect(() =>{
-        const handleUsers = () =>{
-           if(records){
-            setUsers(records);
-           }
+    useEffect(() => {
+        const handleUsers = () => {
+            if (records) {
+                setUsers(records);
+            }
         }
         handleUsers();
-    },[records])
-    
+    }, [records])
+
     return (
         <div>
             <div className={style.nav__menu__icon}>
@@ -58,44 +58,44 @@ const LeaderBoard = (props: Props) => {
                 </button>
             </div>
             <div className={sidebar ? style.nav__menu__active : style.nav__menu}>
-            <div className={style.result}>
-                <div className={style.result__title}>
-                    Wall of fame
-                </div>
-                <div className={style.result__category}>
-                    <select name="" id="">
-                        <option value="1">Basic Operations</option>
-                        <option value="2">Additions (+)</option>
-                    </select>
-                </div>
-
-                {
-                    users.map((item,index) => (
-                        <div key={item._id} className={style.result__value}>
-                        <div className="">
-                            {
-                                sortUsers(index)
-                            }
-                        </div>
-                        <div className="-mt-1">
-                            <div className={style.result__username}>
-                                {item.userName}
-                            </div>
-                            <div className={style.result__stat}>
-                                <div className={style.result__percent}>
-                                    {item.error} %
-                                </div>
-                                <div className={style.result__time}>
-                                    {item.timelapse}
-                                </div>
-                            </div>
-                        </div>
+                <div className={style.result}>
+                    <div className={style.result__title}>
+                        Wall of fame
                     </div>
-                    ))
-                }
+                    <div className={style.result__category}>
+                        <select name="" id="">
+                            <option value="1">Basic Operations</option>
+                            <option value="2">Additions (+)</option>
+                        </select>
+                    </div>
+
+                    {
+                        users.map((item, index) => (
+                            <div key={item._id} className={style.result__value}>
+                                <div className="">
+                                    {
+                                        sortUsers(index)
+                                    }
+                                </div>
+                                <div className="-mt-1">
+                                    <div className={style.result__username}>
+                                        {item.userName}
+                                    </div>
+                                    <div className={style.result__stat}>
+                                        <div className={style.result__percent}>
+                                            {item.error} %
+                                        </div>
+                                        <div className={style.result__time}>
+                                            {item.timelapse}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
