f8a6d897d805172b0992e4d282b65626c4d563de
vasloc202 <nvloc2510z@gmail.com>
2022-08-04 10:46:22 +0700

    Draft: FA-62 custom border

--- src/components/TableCalculator/index.tsx
+++ src/components/TableCalculator/index.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { roundTo2, numDot, l100, avgOfArray, removecommas } from "../../commons/index";
+import {
+    roundTo2,
+    numDot,
+    l100,
+    avgOfArray,
+    removecommas,
+} from "../../commons/index";
 import { randomCalculation, setInput } from "@/store/slice/calculationSlice";
 import { randomNumber } from "@/store/slice/numberSlice";
 import {
@@ -12,16 +18,15 @@ import { saveTotal } from "@/store/slice/totalSlice";
 import Swal from "sweetalert2";
 import Link from "next/link";
 import { useRecords } from "@/hooks/records";
-import { currencyMask } from "@/commons/index"
+import { currencyMask } from "@/commons/index";
 import { isFulfilled } from "@reduxjs/toolkit";
 
 type Props = {
     percent: number;
     setStart: () => void;
-    inputRef:any
 };
 
-const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
+const TableCalculator = ({ percent, setStart }: Props) => {
     const [dot, setDot] = useState({ answer: "" });
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setDot({ ...dot, [e.target.name]: e.target.value });
@@ -38,7 +43,6 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
     const avgTime = useSelector((state: any) => state.result.avgTime);
 
     const { UseAddRecord } = useRecords();
-
     // Get user
 
     var user;
@@ -54,17 +58,17 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
         // })
     }
 
-    
-
     // Danh sách phép tính
     const calculationList = useSelector(
         (state: any) => state.calculation.calculations
     );
-    
-    const ref = useRef(null)
+
+    const ref = useRef(null);
     // Phép tính option
 
-    const optionCalculation = useSelector((state:any) => state.calculation.addition);
+    const optionCalculation = useSelector(
+        (state: any) => state.calculation.addition
+    );
 
     const Operations = useSelector((state: any) => state.calculation.Operations);
 
@@ -80,7 +84,6 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                 /* Read more about isConfirmed, isDenied below */
                 if (result.isConfirmed) {
                     if (!localStorage.getItem("user")) {
-                    
                     } else {
                         UseAddRecord({
                             userId: user.user._id,
@@ -107,7 +110,6 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
     const dispatch = useDispatch();
 
     // Value input
-    
 
     // Phép tính
     const s = useSelector((state: any) => state.calculation.calculation);
@@ -171,27 +173,26 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
             dispatch(saveDuration(lapNum));
             setLap([...lap, lapNum]);
             dispatch(saveStart(Date.now()));
-            setTime(
-                lapText.getSeconds() +
-                "s" +
-                lapText.getMilliseconds()
-            );
+            setTime(lapText.getSeconds() + "s" + lapText.getMilliseconds());
         } else {
             console.log("Lap Array: ", lap);
             console.log("Time average: ", Math.ceil(avgOfArray(lap)));
         }
     };
     const handleKeyDown = (event) => {
-        
-        if((event.which >= 65 && event.which < 96) || (event.which > 105 && event.which <= 255 && event.which != 190)){
+        if (
+            (event.which >= 65 && event.which < 96) ||
+            (event.which > 105 && event.which <= 255 && event.which != 190)
+        ) {
             Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Vui lòng vui lòng không nhập gì khác ngoài số!',
-            })
+                icon: "error",
+                title: "Oops...",
+                text: "Vui lòng vui lòng không nhập gì khác ngoài số!",
+            });
         }
         if (event.which >= 37 && event.which <= 40) return; // arrow
-        if (event.which >= 48 && event.which <= 57) dispatch(setInput(event.target.value)) ;
+        if (event.which >= 48 && event.which <= 57)
+            dispatch(setInput(event.target.value));
         if (event.keyCode === 13) {
             Lap();
             if (s == "+") {
@@ -225,12 +226,12 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
             }
 
             // Tất cả phép tính
-            if(optionCalculation == 1){
+            if (optionCalculation == 1) {
                 if (s == "+") {
                     const c = a + b;
                     logicCalculation(c);
                 }
-    
+
                 if (s == "-") {
                     if (a < b) {
                         const c = b - a;
@@ -240,12 +241,12 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                         logicCalculation(c);
                     }
                 }
-    
+
                 if (s == "×") {
                     const c = a * b;
                     logicCalculation(c);
                 }
-    
+
                 if (s == "÷") {
                     if (a < b) {
                         const c = b / a;
@@ -257,12 +258,12 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                 }
             }
             // Phép tính cộng
-            else if(optionCalculation == 2){
+            else if (optionCalculation == 2) {
                 const c = a + b;
                 logicCalculation(c);
             }
             // Phép tính trừ
-            else if(optionCalculation == 3){
+            else if (optionCalculation == 3) {
                 if (a < b) {
                     const c = b - a;
                     logicCalculation(c);
@@ -272,7 +273,7 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                 }
             }
             // Phép tính nhân
-            else if(optionCalculation == 4){
+            else if (optionCalculation == 4) {
                 const c = a * b;
                 logicCalculation(c);
             }
@@ -290,8 +291,8 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
         }
     };
     useEffect(() => {
-        if(calculation.length){
-            ref.current.focus()
+        if (calculation.length) {
+            ref.current.focus();
         }
         if (removecommas(input) != 0) {
             dispatch(
@@ -308,7 +309,7 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
         }
     }, [sum, calculation.length]);
     console.log(input);
-    
+
     // Đóng thông báo sai
     const close = () => {
         setShowTotal(false);
@@ -319,24 +320,69 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
             {notification ? (
                 showTotal ? (
                     <div className="w-full text-white bg-red-500 ">
-                         <div id="popup-modal" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-                        <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
-                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                    <span className="sr-only">Close modal</span>
-                                </button>
-                                <div className="p-6 text-center">
-                                    <svg aria-hidden="true" className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                                    <button data-modal-toggle="popup-modal" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                        Yes, Im sure
+                        <div
+                            id="popup-modal"
+                            className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full"
+                        >
+                            <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+                                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <button
+                                        type="button"
+                                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                                        data-modal-toggle="popup-modal"
+                                    >
+                                        <svg
+                                            aria-hidden="true"
+                                            className="w-5 h-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                        <span className="sr-only">Close modal</span>
                                     </button>
-                                    <button data-modal-toggle="popup-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                                    <div className="p-6 text-center">
+                                        <svg
+                                            aria-hidden="true"
+                                            className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                            ></path>
+                                        </svg>
+                                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                            Are you sure you want to delete this product?
+                                        </h3>
+                                        <button
+                                            data-modal-toggle="popup-modal"
+                                            type="button"
+                                            className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                                        >
+                                            Yes, Im sure
+                                        </button>
+                                        <button
+                                            data-modal-toggle="popup-modal"
+                                            type="button"
+                                            className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                                        >
+                                            No, cancel
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                         <div className="container flex items-center justify-between px-6 py-4 mx-auto">
                             <div className="flex">
                                 <svg viewBox="0 0 40 40" className="w-6 h-6 fill-current">
@@ -405,7 +451,8 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                                         <td>
                                             {item.marginOfError == 0
                                                 ? "0"
-                                                : l100(roundTo2(item.marginOfError))}%
+                                                : l100(roundTo2(item.marginOfError))}
+                                            %
                                         </td>
                                         <td>{item.duration == 0 ? "" : <div>{item.time}</div>}</td>
                                     </tr>
@@ -495,7 +542,8 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                                                 >
                                                     {data.marginOfError == 0
                                                         ? ""
-                                                        : l100(roundTo2(data.marginOfError))} %
+                                                        : l100(roundTo2(data.marginOfError))}{" "}
+                                                    %
                                                 </td>
                                                 <td
                                                     className="absolute"
@@ -512,7 +560,7 @@ const TableCalculator = ({ percent, setStart, inputRef}: Props) => {
                     </table>
                 </div>
             )}
-                                    
+
             <hr className="py-4" />
         </div>
     );
